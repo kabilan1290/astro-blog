79a05ac987dade372dcb9f9eeb9172d27769f5f2
Kabilan S <tamiltuts1290@gmail.com>
2025-07-29 14:41:11 +0530

    AI research

--- src/consts.ts
+++ src/consts.ts
@@ -3,13 +3,13 @@
 
 export const SITE_TITLE = 'Game0v3r Blog';
 
-export const SITE_DESCRIPTION = 'நான் வீழ்வேனென்று நினைத்தாயோ?';
+export const SITE_DESCRIPTION = 'A random guy on internet writing about information security...';
 
 export const FAVICON_SRC = '/favicon.jpeg';
 
 export const AVATAR_SRC = '/dp.jpeg';
 
-export const BIO = "A random guy on internet writing about information security"
+export const BIO = "நான் வீழ்வேனென்று நினைத்தாயோ?"
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
 
